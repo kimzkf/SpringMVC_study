a93ed615a731d15a1f93621d1e6a8c9612a7a024
zhang_k <13612085393@163.com>
2019-04-04 11:27:38 +0800

    RequestBody

--- src/main/java/cn/zkf/controller/AnnoController.java
+++ src/main/java/cn/zkf/controller/AnnoController.java
@@ -1,6 +1,7 @@
 package cn.zkf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -14,6 +15,18 @@ public class AnnoController {
     public String testRequestParams(@RequestParam(value = "username") String name){
         System.out.println("testRequestParams执行了。。。");
         System.out.println(name);
+        return  "success";
+    }
+
+    /**
+     * 获取到请求体内容
+     * @param name
+     * @return
+     */
+    @RequestMapping(path = "/testRequestBody")
+    public String testRequestBody(@RequestBody(required = true) String body){
+        System.out.println("testRequestBody执行了。。。");
+        System.out.println(body);
         return  "success";
     }
 }
--- src/main/webapp/anno.jsp
+++ src/main/webapp/anno.jsp
@@ -13,5 +13,26 @@
 <body>
 <%--常用注解--%>
      <a href="anno/testRequestParams?username=hahah">RequestParams</a>
+<hr/>
+<h3>RequestBody获取请求体</h3>
+<form action="anno/testRequestBody" method="post">
+    <table>
+        <tr>
+            <td>username</td>
+            <td><input type="text" name="username" value=""/></td>
+        </tr>
+        <tr>
+            <td>password</td>
+            <td><input type="text" name="password" value=""/></td>
+        </tr>
+        <tr>
+            <td>date</td>
+            <td><input type="date"  name="date" value=""/></td>
+        </tr>
+        <tr>
+            <td><input type="submit" value="submit"></td>
+        </tr>
+    </table>
+</form>
 </body>
 </html>
